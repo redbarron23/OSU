8cd686132e111d63bc632c984300755cba451a53
James Hourihane <jhourihane@gmail.com>
2014-07-12 11:55:31 -0700

    checking in a3e3_SimpleRandomNumber.cpp

--- CS161/03/a3e3_SimpleRandomNumber.cpp
+++ CS161/03/a3e3_SimpleRandomNumber.cpp
@@ -4,5 +4,38 @@
 * and any searches you want (including the book if you can find something good in there :) ),
 * b. Print some random numbers using a loop (say 10 of them),
 
-* Discuss: Run the program a few times and see what happens, did you get the same numbers multiple times? What is happening here? Can we fix this?
+* Discuss: Run the program a few times and see what happens, 
+* did you get the same numbers multiple times? 
+* What is happening here? Can we fix this?
 */
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+/*
+ * Doesn't seem to random
+ * Can we make this more random than it is?
+ */
+
+int main() {
+
+    int myRandomNum;
+
+    /* initialize random seed: */
+    srand (time(NULL));
+
+    /* generate secret number between 1 and 10: */
+
+    //cout << myRandomNum << endl;
+
+    for (int i=0; i< 11; i++) {
+        myRandomNum = rand() % 10 + 1;
+        cout << myRandomNum << endl;
+    }
+
+
+}
+
