81428b6c91d1e251635366e194a7b1584db28285
James Hourihane <jhourihane@gmail.com>
2014-07-17 22:53:23 -0700

    bare bones

--- CS161/04/size.cpp
+++ CS161/04/size.cpp
@@ -1,3 +1,4 @@
+/*
 4. (4) Programming Project 3.5 from Absolute C++ (p138 in the 5e book),
 File must be called: size.cpp
 “Write a program that asks for the user’s height, weight, and age, and then computes clothing sizes according to the following formulas.
@@ -5,4 +6,34 @@ File must be called: size.cpp
 • Jacket size (chest in inches) =height times weight divided by 288 and then adjusted by adding one- eighth of an inch for each 10 years over age 30. (Note that the adjustment only takes place after a full 10 years. So, there is no adjustment for ages 30 through 39, but one-eighth of an inch is added for age 40.)
 • Waist in inches =weight divided by 5.7 and then adjusted by adding one-tenth of an inch for each 2 years over age 28. (Note that the adjustment only takes place after a full 2 years. So, there is no adjustment for age 29, but one-tenth of an inch is added for age 30.)
 Use functions for each calculation. Your program should allow the user to repeat this calculation as often as he or she wishes.”
+
+* Some Comments
+* more Comments
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+
+}
+
+userInput {
+    cout << Please enter your height, weight, and age" << endl;
+
+}
+
+clothingSize {
+    float height, 
+    int weight, 
+    int age; 
+    int hatSize = ((weight / height) * 2.9);
+    int jacketSize;
+    int waist = weight / 5.7;
+}
+
+
 
