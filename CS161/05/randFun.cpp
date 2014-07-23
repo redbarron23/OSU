42e514b4188947272218c010f262923ac0486a76
James Hourihane <jhourihane@gmail.com>
2014-07-23 14:26:13 -0700

    input only at this point

--- CS161/05/randFun.cpp
+++ CS161/05/randFun.cpp
@@ -9,16 +9,53 @@ a. its definition looks like the following: void rand_int(const int &min, const 
 b. it accepts references to two int values and a reference to a third value (one for the bottom of the random range, one for the top of the random range, and one to store the final value), generates a number within that range, and then sets the value of the third number to that generated value,
 Discuss:
 i. Why might I use const in the signature of the function?
+Answer:
 ii. Why might I want to do this with references to values instead of copies of values?
-iii. Can I return two values from a function? Is there a way to alter multiple values passed into a function
-call?
+Answer:
+
+iii. Can I return two values from a function? Is there a way to alter multiple values passed into a function call?
+Answer:
+
+
 */
 
 #include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
+
+void getNumbers(int& min, int& max);
+//void rand_int(const int& min, const int& max, int& val);
+//void showResults(int val);
 
 int main()
 {
-
-
+    int firstNum, secondNum;
+    getNumbers(firstNum, secondNum);
+    //rand_int(min, max, val);
+    //showResults(rand_int);
+    
+    return 0;
 }
+
+void getNumbers(int& min, int& max)
+{
+    cout << "Please enter a min number: " << endl;
+    cin >> min;
+    cout << "Please enter a max number: " << endl;
+    cin >> max;
+}
+
+void rand_int(const int& min, const int& max, int& val)
+{
+    srand (time(NULL));
+    val = rand() % max + min;
+}
+
+void showResults(int val)
+{
+    cout << "Random number is: "
+         << val << endl;
+}
+
