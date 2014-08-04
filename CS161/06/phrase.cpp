332831b602c6855f99d1a15529f9a2e49a892471
James Hourihane <jhourihane@gmail.com>
2014-08-03 18:54:45 -0700

    getting some shape to it

--- CS161/06/phrase.cpp
+++ CS161/06/phrase.cpp
@@ -5,30 +5,73 @@
  * desc:  create an array of words have user guess the word one string at a time
  * file:  phrase.cpp
 */
-
-/*
- *for(int k = 0; k < secretWord.length(); k++) //Locating the guessed letter in the hidden string
-{
-
-   if(letterIn == letterBank[k])
-   {
-      letterBank[k] = '-';
-   }
-}
-*/
-
 #include <iostream>
 #include <string>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 using namespace std;
 
-// a. Create an array of words consisting only of letters
+//a. Create an array of words consisting only of letters
+void listValidCharacters();
+
 // b. Select a phrase by randomly selecting a number of words from your array of words
-// c. Ask the user to guess the secret phrase, one letter at a time
-// Show the user what (if any) of the phrase has been correctly guessed so far, 
 // Note: perhaps use underscores or hyphens to denote   letters not yet guessed,
+void selectPhrase();
+
+//c. Ask the user to guess the secret phrase, one letter at a time
+void guessMyPhrase();
+
+// Show the user what (if any) of the phrase has been correctly guessed so far, 
+void printArray(int array[], int size);
 
 int main()
 {
-   cout << "Hello" << endl;
-   return 0; 
+    listValidCharacters();  //
+    guessMyPhrase();
+
+    for(int i=0; i < sizeof(arrayOfWords); i++)
+     {
+         cout << arrayOfWords[i] << endl;
+     }
+
+    printArray(myArray, ARRAY_SIZE);
+
+    return 0;
+
+}
+
+void listValidCharacters()
+{
+    cout << "Valid Characters are: [a-z] or [A-Z]   such as:  ABC or abc or Abc" << endl;
+
+}
+
+void guessMyPhrase()
+{
+    char guess;
+    int arrayOfWordsSize = 13;
+    srand (time(NULL));
+    int randomVar = rand()%arrayOfWordsSize;
+    //cout << randomVar << endl;
+    //char arrayOfWords[] = {"I" "Love" "Summer!"};
+    char arrayOfWords[] = "I Love Summer!";
+    //Ask the user to guess the secret phrase, one letter at a time
+    cout << "Guess the secret phrase: Please enter a character" << endl;
+    cin >> guess;
+
+}
+
+void printArray(int array[], int size){
+
+    static int printCount = 0;
+
+    //loop for moving through each element of the array
+    for(int i = 0; i < size; i++){
+        if(printCount == 0){
+            cout << "The element value of myArray " << (i + 1) << " is: " << array[i] << endl;
+        }
+        else{
+            cout << "\nThe element value of myNextArray " << (i + 1) << " is: " << array[i];
+        }
+    }
 }
