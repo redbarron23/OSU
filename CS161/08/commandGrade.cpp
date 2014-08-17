3d8fce17780b1dcb088c3b87d37253c9df9920e5
James Hourihane <jhourihane@gmail.com>
2014-08-16 20:18:52 -0700

    cleaned up and added comments

--- CS161/08/commandGrade.cpp
+++ CS161/08/commandGrade.cpp
@@ -20,7 +20,9 @@ can be).
 #include <iostream>
 using namespace std;
 
-int main()
-{
-
+int main(int argc, char* argv[]) { 
+   cout << "argc = " << argc << endl; 
+   for(int i = 0; i < argc; i++) 
+      cout << "argv[" << i << "] = " << argv[i] << endl; 
+   return 0; 
 }
--- CS161/08/gradeClass.cpp
+++ CS161/08/gradeClass.cpp
@@ -5,15 +5,6 @@
  * Desc: input and ouput student's AverageScore and Final letter grade based on quiz, midterm and final
  */
 
-/*
- * (4) Programming Project 6.1 from Absolute C++ (p271 in the 5e book),
- * 1. Write a grading program for a class with the following grading policies:
-a. There are two quizzes, each graded on the basis of 10 points.
-b. There is one midterm exam and one final exam, each graded on the basis of 100 points.
-c. The final exam counts for 50% of the grade, the midterm counts for 25%, and the two quizzes together count for a total of 25%. (Do not forget to normalize the quiz scores. They should be converted to a percentage before they are aver- aged in.)
-Any grade of 90 or more is an A, any grade of 80 or more (but less than 90) is a B, any grade of 70 or more (but less than 80) is a C, any grade of 60 or more (but less than 70) is a D, and any grade below 60 is an F. The program will read in the student’s scores and output the student’s record, which consists of two quiz and two exam scores as well as the student’s average numeric score for the entire course and final letter grade. Define and use a structure for the student record.
- */
-
 /* weight of grades
  * quiz1 = 10 pts    .. ...........//12.5% 
  * quiz2 = 10 pts    ............  //12.5% 
@@ -36,6 +27,12 @@ struct studentRecord
    //string finalLetterGrade;
 };
 
+/*
+ * 4 terms are dynamic (quiz1, quiz2, midTerm and final)
+ * and 1 is calculated upon the weighted sum
+ * afterwards the  struct is passed by value
+ */
+
 studentRecord newStudentRecord ( )
 {
     studentRecord myStudentRecord; // intialize studentRecord to be returned
@@ -56,11 +53,10 @@ studentRecord newStudentRecord ( )
     // weighted total of quiz, midterm& final
     // http://www.blacksdomain.com/files/Notes/Calculating_WA.php
     // also need to stick the finalLetterGrade back into the same struct; 
+    // I decided against this as it was unecssary
     double weightedSum = (myStudentRecord.quiz1 * .125) + (myStudentRecord.quiz2 * .125) + (myStudentRecord.midTerm * .25) + (myStudentRecord.final * .50);
-    myStudentRecord.averageNumericScore = weightedSum;
+    myStudentRecord.averageNumericScore = weightedSum; // this is the final value
    
-   
-
     return(myStudentRecord); // return by value
 }
 
