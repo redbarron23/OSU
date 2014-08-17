f17f334a5f74bb1e3f4b048559f1c90b3fd7b622
James Hourihane <jhourihane@gmail.com>
2014-08-16 20:10:56 -0700

    gradeClass.cpp

--- CS161/08/gradeClass.cpp
+++ CS161/08/gradeClass.cpp
@@ -31,56 +31,59 @@ struct studentRecord
    double midTerm;
    double final;
    double averageNumericScore;
+   // maybe reassign this in main
    //char finalLetterGrade[4];
-   string finalLetterGrade;
+   //string finalLetterGrade;
 };
 
 studentRecord newStudentRecord ( )
 {
-    studentRecord myStudentRecord; // studentRecord to be returned
+    studentRecord myStudentRecord; // intialize studentRecord to be returned
 
     cout << "Enter the person's quiz1: ";
     //cin.get >> myStudentRecord.quiz1;
     cin  >> myStudentRecord.quiz1;
 
     cout << "Enter the person's quiz2: ";
-    //cin.get >> myStudentRecord.quiz2;
     cin >> myStudentRecord.quiz2;
 
     cout << "Enter the person's midTerm: ";
-    //cin.get >> myStudentRecord.midTerm;
     cin >> myStudentRecord.midTerm;
 
     cout << "Enter the person's final: ";
-    //cin.get >> myStudentRecord.final;
     cin >> myStudentRecord.final;
 
-   // add up all the scores here and weight them
-   // also need to stick the finalLetterGrade back into the same struct; 
+    // weighted total of quiz, midterm& final
+    // http://www.blacksdomain.com/files/Notes/Calculating_WA.php
+    // also need to stick the finalLetterGrade back into the same struct; 
+    double weightedSum = (myStudentRecord.quiz1 * .125) + (myStudentRecord.quiz2 * .125) + (myStudentRecord.midTerm * .25) + (myStudentRecord.final * .50);
+    myStudentRecord.averageNumericScore = weightedSum;
+   
+   
 
-    return(myStudentRecord);
+    return(myStudentRecord); // return by value
 }
 
 int main()
 {
-     studentRecord myStudentRecord = newStudentRecord();
+     studentRecord myStudentRecord = newStudentRecord();  //init new Record for Student
      //studentRecord myStudentRecord;
      // if (0 <= number && number <= 10)
      //
      // Hopefully not over 100 no test condition for that yet
-     if (myStudentRecord.finalLetterGrade > 90.0 && myStudentRecord.finalLetterGrade < 101.0)
+     if (myStudentRecord.averageNumericScore > 90.0 && myStudentRecord.averageNumericScore < 101.0)
      {
          cout << "student got an A" << endl;
      }
-     else if (myStudentRecord.finalLetterGrade > 80.0 && myStudentRecord.finalLetterGrade < 90.0)
+     else if (myStudentRecord.averageNumericScore > 80.0 && myStudentRecord.averageNumericScore < 90.0)
      {
          cout << "student got an B" << endl;
      }
-     else if (myStudentRecord.finalLetterGrade > 70.0 && myStudentRecord.finalLetterGrade < 80.0)
+     else if (myStudentRecord.averageNumericScore > 70.0 && myStudentRecord.averageNumericScore < 80.0)
      {
          cout << "student got an C" << endl;
      }
-     else if (myStudentRecord.finalLetterGrade > 60.0 && myStudentRecord.finalLetterGrade < 70.0)
+     else if (myStudentRecord.averageNumericScore > 60.0 && myStudentRecord.averageNumericScore < 70.0)
      {
          cout << "student got an D" << endl;
      }
